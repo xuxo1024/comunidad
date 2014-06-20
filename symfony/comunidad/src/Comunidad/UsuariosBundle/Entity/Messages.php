8310523e0960d90e32da1f7fb18eb0c921fb48e2
xuxo1024 <xuxo@xuxo.net>
2014-06-20 14:02:52 +0200

    entidades de doctrine
    creación de entidades de doctrine

---
+++ symfony/comunidad/src/Comunidad/UsuariosBundle/Entity/Messages.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Comunidad\UsuariosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Messages
+ *
+ * @ORM\Table(name="messages", indexes={@ORM\Index(name="id_user", columns={"id_user"})})
+ * @ORM\Entity
+ */
+class Messages
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="to_user", type="integer", nullable=false)
+     */
+    private $toUser;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="blob", nullable=false)
+     */
+    private $message;
+
+    /**
+     * @var \Comunidad\UsuariosBundle\Entity\Users
+     *
+     * @ORM\ManyToOne(targetEntity="Comunidad\UsuariosBundle\Entity\Users")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_user", referencedColumnName="id")
+     * })
+     */
+    private $idUser;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set toUser
+     *
+     * @param integer $toUser
+     * @return Messages
+     */
+    public function setToUser($toUser)
+    {
+        $this->toUser = $toUser;
+
+        return $this;
+    }
+
+    /**
+     * Get toUser
+     *
+     * @return integer 
+     */
+    public function getToUser()
+    {
+        return $this->toUser;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Messages
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set idUser
+     *
+     * @param \Comunidad\UsuariosBundle\Entity\Users $idUser
+     * @return Messages
+     */
+    public function setIdUser(\Comunidad\UsuariosBundle\Entity\Users $idUser = null)
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get idUser
+     *
+     * @return \Comunidad\UsuariosBundle\Entity\Users 
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+}
---
+++ symfony/comunidad/src/Comunidad/UsuariosBundle/Entity/Users.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Comunidad\UsuariosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity
+ */
+class Users
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255, nullable=false)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=false)
+     */
+    private $password;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     * @return Users
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string 
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Users
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
