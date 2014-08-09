4a52160799959a333b6819e8c2e96b2d4f5d91b0
jesus.suero <jesus.suero@tantacom.com>
2014-08-09 17:47:10 +0200

    ejercicio 8.2

--- symfony/comunidad/src/Comunidad/UsuariosBundle/Controller/AdminController.php
+++ symfony/comunidad/src/Comunidad/UsuariosBundle/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace Comunidad\UsuariosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class AdminController extends Controller
 {
@@ -12,7 +13,30 @@ class AdminController extends Controller
         return $this->render('UsuariosBundle:Admin:index.html.twig');
     }
     
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
 
+        // obtiene el error de inicio de sesión si lo hay
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'UsuariosBundle:Admin:login.html.twig',
+            array(
+                // último nombre de usuario ingresado
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+            )
+        );
+    }
     
 
 }
